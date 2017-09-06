933e7ce0d1194e9f91a4ff248e083e595087de8f
Wadageri, Susheelkumar <Susheelkumar_Wadageri@intuit.com>
2017-09-06 11:47:07 +0530

    updating pom.xml with testng and maven plugin dependencies

--- src/test/java/com/actitime/tests/SigninTest.java
+++ src/test/java/com/actitime/tests/SigninTest.java
@@ -4,21 +4,36 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.actitime.page.LoginPage;
 
 public class SigninTest extends BaseClass {
 	
+	@Parameters({"browserType"})
 	@Test
-	public void signInTest(){
-		String browserType="chrome";
+	
+	public void signInTest(String sBrowserType){
+		String browserType= sBrowserType;
 		String os="mac";
-		
-		if(browserType.equalsIgnoreCase("Chrome") && (os.equalsIgnoreCase("mac")))
+		if(browserType.equalsIgnoreCase("firefox") && (os.equalsIgnoreCase("mac")))
 		{
-		System.setProperty("webdriver.chrome.driver","/Users/swadageri/Actitime_UI_Automation/src/test/resources/chromedriver");
-		WebDriver driver=new ChromeDriver();
+			System.setProperty("webdriver.gecko.driver","/Users/swadageri/Actitime_UI_Automation/src/test/resources/geckodriver");
+			WebDriver driver=new FirefoxDriver();
+			this.driver=driver;
+
+		}
+		
+		else if(browserType.equalsIgnoreCase("chrome") && (os.equalsIgnoreCase("mac")))
+		{
+			System.setProperty("webdriver.chrome.driver","/Users/swadageri/Actitime_UI_Automation/src/test/resources/chromedriver");
+			WebDriver driver=new ChromeDriver();
+			this.driver=driver;
+		}
+
+		
 		driver.get("https://online.actitime.com/some/login.do");
 	
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -29,4 +44,4 @@ public class SigninTest extends BaseClass {
 
 	}
 
-	}}
+	}
